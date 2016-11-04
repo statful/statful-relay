6fc319664ff14ed7476827ae0b1b0e2e4dede7e4
Bruno Palos <brunopalos@gmail.com>
2016-11-04 11:56:11 +0000

    fix(NA): Remove platform dependent Sleep and use mocha done sync function for bootstrapping tests.

--- tests/integration/specs/root.spec.js
+++ tests/integration/specs/root.spec.js
@@ -2,18 +2,18 @@ var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 var expect = require('chai').expect;
-
-var sleep = require('sleep');
 var mockTool = require('../setup/mock-tool');
 
 var testRelay;
 /*jshint -W117 */
-before(function () {
+before(function (done) {
     testRelay = require('../setup/bootstrap-relay');
 
     // Wait for Relay to start
     // TODO - change this read an healthcheck
-    sleep.sleep(1);
+    setTimeout(function() {
+        done();
+    }, 500);
 });
 
 after(function () {
