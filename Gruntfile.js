f25d7b01a0c56aada8a4250f0cadac207cbf5e84
Jose Tavares <jose.tavares@mindera.com>
2017-08-21 17:35:17 +0100

    chore: simply build process, use eslint, remove unused tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,6 @@
 'use strict';
 
-var mockserverConf = require('./tests/integration/conf/mockserver.json');
-
 module.exports = function (grunt) {
-
-    // Command line options
-    var testSuite = grunt.option('testSuite') || 'statful';
-
-    var targetHost = grunt.option('targetHost') || 'localhost';
-    var statfulPort = grunt.option('statfulPort') || 2013;
-    var collectdPort = grunt.option('collectdPort') || 2023;
-
-    var durationMs = grunt.option('durationMs') || 60000;
-    var clientsCount = grunt.option('clientsCount') || 200;
-    var metricsPerRequest = grunt.option('metricsPerRequest') || 70;
-    var cycleDurationMs = grunt.option('cycleDurationMs') || 10000;
-
-    // Show elapsed time at the end
-
-    require('time-grunt')(grunt);
-
     // Load all grunt tasks
     require('load-grunt-tasks')(grunt);
 
@@ -32,44 +13,6 @@ module.exports = function (grunt) {
 
         buildNumber: process.env.GO_PIPELINE_COUNTER || process.env.BUILD_NUMBER || 1,
 
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc',
-                reporter: require('jshint-stylish')
-            },
-            gruntfile: {
-                src: ['Gruntfile.js']
-            },
-            js: {
-                src: ['*.js', 'lib/**/*.js']
-            },
-            test: {
-                src: ['tests/**/*.js']
-            }
-        },
-        mochacli: {
-            options: {
-                reporter: 'nyan',
-                bail: true,
-                timeout: 10000
-            },
-            integration: ['tests/integration/specs/**/*.spec.js'],
-            performance: {
-                options: {
-                    env: {
-                        timeout: 1800000,
-                        durationMs: durationMs,
-                        clientsCount: clientsCount,
-                        metricsPerRequest: metricsPerRequest,
-                        cycleDurationMs: cycleDurationMs,
-                        targetHost: targetHost,
-                        collectdPort: collectdPort,
-                        statfulPort: statfulPort
-                    }
-                },
-                src: ['tests/performance/specs/' + testSuite + '.spec.js']
-            }
-        },
         watch: {
             gruntfile: {
                 files: '<%= jshint.gruntfile.src %>',
@@ -83,42 +26,15 @@ module.exports = function (grunt) {
                 files: '<%= jshint.test.src %>',
                 tasks: ['jshint:test', 'mochacli']
             }
-        },
-        start_mockserver: {
-            options: {
-                serverPort: mockserverConf.port,
-                verbose: true
-            }
-        },
-        stop_mockserver: {
-            options: {
-                serverPort: mockserverConf.port,
-                verbose: true
-            }
         }
     });
-
 
     grunt.registerTask('dev', [
         'watch'
     ]);
 
-    grunt.registerTask('integration-test', [
-        'start_mockserver',
-        'continue:on',
-        'mochacli:integration',
-        'continue:off',
-        'stop_mockserver',
-        'continue:fail-on-warning'
-    ]);
-
-    grunt.registerTask('performance-test', [
-        'mochacli:performance'
-    ]);
-
     grunt.registerTask('test', [
         'jshint'
-        //'integration-test'
     ]);
 
     grunt.registerTask('default', [
