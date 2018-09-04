95f5f769653885e0f337b7697289cd1ac89c99a1
Jose Tavares <josetavares00@gmail.com>
2018-09-04 17:01:39 +0100

    fix: remove Grunt,format code

--- cli/commands.js
+++ cli/commands.js
@@ -5,12 +5,12 @@ var fs = require('fs-extra');
 var path = require('path');
 var pm2 = require('pm2');
 
-var generateConfig = function(configPath) {
+var generateConfig = function (configPath) {
     var normalizedPath = path.normalize(configPath);
     var source = path.normalize(__dirname + '/conf/defaults.json');
     var target = path.join(normalizedPath, 'statful-relay-conf.json');
 
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
         fs.copy(source, target, function (err) {
             if (err) {
                 reject(err);
@@ -21,12 +21,12 @@ var generateConfig = function(configPath) {
     });
 };
 
-var start = function(configPath) {
+var start = function (configPath) {
     var configToTry = new Config(configPath);
 
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
         configToTry.load().then(
-            function(loadedConfig) {
+            function (loadedConfig) {
                 var statfulClient = new Statful(loadedConfig.statfulClient);
                 var statfulRelay = new Relay(loadedConfig, statfulClient);
 
@@ -41,70 +41,27 @@ var start = function(configPath) {
                 } else {
                     reject('Error starting Statful Relay with given configuration.');
                 }
-
             },
-            function(error) {
+            function (error) {
                 reject(error);
             }
         );
     });
 };
 
-var startManaged = function(configPath) {
-    return new Promise(function(resolve, reject) {
-        pm2.connect(function(err) {
+var startManaged = function (configPath) {
+    return new Promise(function (resolve, reject) {
+        pm2.connect(function (err) {
             if (err) {
                 reject(err);
             } else {
-                pm2.start({
-                    name: 'statful-relay',
-                    script: 'statful-relay',
-                    args: 'start ' + configPath
-                }, function(err) {
-                    pm2.disconnect();
-
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve();
-                    }
-                });
-            }
-        });
-
-
-    });
-};
-
-var stopManaged = function() {
-    return new Promise(function(resolve, reject) {
-        pm2.connect(function(err) {
-            if (err) {
-                reject(err);
-            } else {
-                pm2.stop('statful-relay',
-                    function(err) {
-                    pm2.disconnect();
-
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve();
-                    }
-                });
-            }
-        });
-    });
-};
-
-var restartManaged = function() {
-    return new Promise(function(resolve, reject) {
-        pm2.connect(function(err) {
-            if (err) {
-                reject(err);
-            } else {
-                pm2.restart('statful-relay',
-                    function(err) {
+                pm2.start(
+                    {
+                        name: 'statful-relay',
+                        script: 'statful-relay',
+                        args: 'start ' + configPath
+                    },
+                    function (err) {
                         pm2.disconnect();
 
                         if (err) {
@@ -112,7 +69,48 @@ var restartManaged = function() {
                         } else {
                             resolve();
                         }
-                    });
+                    }
+                );
+            }
+        });
+    });
+};
+
+var stopManaged = function () {
+    return new Promise(function (resolve, reject) {
+        pm2.connect(function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                pm2.stop('statful-relay', function (err) {
+                    pm2.disconnect();
+
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            }
+        });
+    });
+};
+
+var restartManaged = function () {
+    return new Promise(function (resolve, reject) {
+        pm2.connect(function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                pm2.restart('statful-relay', function (err) {
+                    pm2.disconnect();
+
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
             }
         });
     });
--- cli/interface.js
+++ cli/interface.js
@@ -1,7 +1,7 @@
 var yargs = require('yargs');
 var Commands = require('./commands');
 
-var Interface = function() {
+var Interface = function () {
     yargs
         .usage('Usage: $0 [command] <path>')
         .command('generate-config <path>', 'Generate a default config for Statful Relay on given path.')
@@ -11,10 +11,19 @@ var Interface = function() {
         .command('restart-managed', 'Restart the Statful Relay, managed by pm2.')
         .demand(1)
         .strict()
-        .example('$0 generate-config /etc/statful-relay/conf', 'Generates a default Statful Relay ' +
-        'config file on /etc/statful-relay/conf/ with name statful-relay-conf.json')
-        .example('$0 start /etc/statful-relay/conf/statful-relay-conf.json', 'Starts the Statful Relay with the given config.')
-        .example('$0 start-managed /etc/statful-relay/conf/statful-relay-conf.json', 'Starts the Statful Relay, managed by pm2, with the given config.')
+        .example(
+            '$0 generate-config /etc/statful-relay/conf',
+            'Generates a default Statful Relay ' +
+                'config file on /etc/statful-relay/conf/ with name statful-relay-conf.json'
+        )
+        .example(
+            '$0 start /etc/statful-relay/conf/statful-relay-conf.json',
+            'Starts the Statful Relay with the given config.'
+        )
+        .example(
+            '$0 start-managed /etc/statful-relay/conf/statful-relay-conf.json',
+            'Starts the Statful Relay, managed by pm2, with the given config.'
+        )
         .example('$0 stop-managed', 'Stops the Statful Relay, managed by pm2.')
         .example('$0 restart-managed', 'Restarts the Statful Relay, managed by pm2.')
         .help('help')
@@ -28,46 +37,50 @@ var Interface = function() {
     if (path) {
         if (argv._[0] === 'start') {
             Commands.start(path).then(
-                function(returnedPath) {
-                    return console.log('Statful Relay successfully loaded with configuration file at \'' + returnedPath + '\'');
+                function (returnedPath) {
+                    return console.log(
+                        "Statful Relay successfully loaded with configuration file at '" + returnedPath + "'"
+                    );
                 },
-                function(error) {
+                function (error) {
                     return console.error(error);
                 }
             );
         } else if (argv._[0] === 'start-managed') {
             Commands.startManaged(path).then(
-                function() {
+                function () {
                     return console.log('Pm2 successfully request a spawn for Statful Relay process.');
                 },
-                function(error) {
+                function (error) {
                     return console.error(error);
                 }
             );
         } else if (argv._[0] === 'stop-managed') {
             Commands.stopManaged().then(
-                function() {
+                function () {
                     return console.log('Pm2 successfully stopped for Statful Relay process.');
                 },
-                function(error) {
+                function (error) {
                     return console.error(error);
                 }
             );
         } else if (argv._[0] === 'restart-managed') {
             Commands.restartManaged().then(
-                function() {
+                function () {
                     return console.log('Pm2 successfully restarted for Statful Relay process.');
                 },
-                function(error) {
+                function (error) {
                     return console.error(error);
                 }
             );
         } else if (argv._[0] === 'generate-config') {
             Commands.generateConfig(path).then(
-                function(returnedPath) {
-                    return console.log('Configuration file \'statful-relay-conf.json\' successfully created at \'' + returnedPath + '\'');
+                function (returnedPath) {
+                    return console.log(
+                        "Configuration file 'statful-relay-conf.json' successfully created at '" + returnedPath + "'"
+                    );
                 },
-                function(error) {
+                function (error) {
                     return console.error(error);
                 }
             );
